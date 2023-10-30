b892572c04d0d1b9a958f81416869db193e7acdd
Migle Sniureviciute <ssmige@gmail.com>
2023-10-30 17:58:47 +0200

    7 paskaita useRef

---
+++ react-7-useRef/useref/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Timer from "./components/Timer/Timer";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
+function App() {
+  return (
+    <div>
+      <VideoPlayer />
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
